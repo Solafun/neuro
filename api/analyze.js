51f9e37f1ee4bd43b4825d80c0834ed6e8e3e2f8
Sola.fun <71711340+Solafun@users.noreply.github.com>
2026-03-24 20:55:31 +0300

    Add files via upload

--- api/analyze.js
+++ api/analyze.js
@@ -307,13 +307,13 @@ async function scrapeThreadsProfile(nickname) {
   }
 }
 
-export const handler = async (event, context) => {
-  if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).send('Method Not Allowed');
   }
 
   try {
-    const { nickname } = JSON.parse(event.body);
+    const nickname = req.body.nickname;
 
     if (!nickname) {
       return res.status(400).json();
